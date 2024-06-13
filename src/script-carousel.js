4d9dcd2ff97c9bf369a76f0f24fd49b3470ef8ab
Sergiy Humenniy <gum0372@gmail.com>
2024-06-13 20:22:52 +0300

    656

--- src/script-carousel.js
+++ src/script-carousel.js
@@ -1,25 +1,19 @@
-
 let currentIndex = 0;
 const carouselTrack = document.querySelector('.carousel-track');
 const totalItems = document.querySelectorAll('.carousel-item').length;
 
 function updateCarousel() {
-    const width = document.querySelector('.carousel').offsetWidth;
-    carouselTrack.style.transform = `translateX(-${currentIndex * width}px)`;
+  const width = document.querySelector('.carousel').offsetWidth;
+  carouselTrack.style.transform = `translateX(-${currentIndex * width}px)`;
 }
 
 function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalItems;
-    updateCarousel();
+  currentIndex = (currentIndex + 1) % totalItems;
+  updateCarousel();
 }
-
 
 setInterval(nextSlide, 2500);
 
-
 function toggleFavorite(element) {
-    element.classList.toggle('active');
+  element.classList.toggle('active');
 }
-
-
-
